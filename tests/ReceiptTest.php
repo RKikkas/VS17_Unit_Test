ac246aebbed5e57036f054a04858521526bb8b65
Roger Kikkas <roger.kikkas@khk.ee>
2019-04-08 10:27:22 +0300

    02 Extend a unit test 01_07

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -13,15 +13,28 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+
+    // setUp loob uue Receipt objekti
+    public function setUp() {
+        $this->Receipt = new Receipt();
+    }
+
+    // tearDown eemaldab pärast teste Receipt objekti
+    public function tearDown() {
+        unset($this->Receipt);
+    }
+
     public function testTotal(){
-        // Loome uue Receipt objekti
-        $Receipt = new Receipt();
+        // array väärtustega
+        $input = [0, 2, 5, 8];
+        // kutsume Receipt meetodi total koos eelnevalt defineeritud väärtustega
+        $output = $this->Receipt->total($input);
         // PHPUniti testi meetod, mis kontrollib kas oodatav väärtus on sama mis reaalne. assertEquals on samaväärne
         // operaator == ja assertSame on ===
         // võtab parameetrid (expected, real value, message in case of error)
         $this->assertEquals(
             15,
-            $Receipt->total([0,2,5,8]),
+            $output,
             'When running should equal 15');
     }
 }
